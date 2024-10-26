577a5933a317c329201082cc56f65e04d1d3cea9
lyin888 <lyin007@gmail.com>
2024-10-26 12:21:40 +0800

    更新配置文件

---
+++ luci-app-gecoosac-pro/luasrc/model/cbi/gecoosac.lua
@@ -0,0 +1,38 @@
+m = Map("gecoosac", translate("Gecoos AC"), translate("Batch management Gecoos AP,Default password:admin"))
+
+m:section(SimpleSection).template  = "gecoosac/gecoosac_status"
+
+s = m:section(TypedSection, "gecoosac", translate("Global Settings"))
+s.addremove = false
+s.anonymous = true
+
+enable = s:option(Flag, "enabled", translate("Enabled AC"))
+enable.rmempty = false
+
+o = s:option(Value, "program_path", translate("Program Path"))
+o.placeholder = "/usr/bin/gecoosac"
+o.default     = "/usr/bin/gecoosac"
+o.description = translate("Executable file path, permission is 0755")
+o.rmempty     = false
+
+o = s:option(Value, "port", translate("Set the AC access port"))
+o.placeholder = 60650
+o.default     = 60650
+o.datatype    = "port"
+o.rmempty     = false
+
+o = s:option(Value, "upload_dir", translate("Upload dir path"), translate("The path to upload AP upgrade firmware"))
+o.placeholder = "/etc/gecoosac/upload/"
+o.default     = "/etc/gecoosac/upload/"
+o.rmempty     = false
+
+o = s:option(Value, "db_dir", translate("Database dir path"), translate("The path to store the config database"))
+o.placeholder = "/etc/gecoosac/"
+o.default     = "/etc/gecoosac/"
+o.rmempty     = false
+
+o = s:option(Flag, "log", translate("Enable Log"))
+o.default = 1
+o.rmempty = false
+
+return m
